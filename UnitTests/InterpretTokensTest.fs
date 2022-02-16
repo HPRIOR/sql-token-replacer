54fc2f8d05cf677f71036872b2b2cb1b6c97924a
Harry Prior <harry.prior@gfk.com>
2022-02-16 18:49:42 +0000

    Update tests to include type

--- UnitTests/InterpretTokensTest.fs
+++ UnitTests/InterpretTokensTest.fs
@@ -6,44 +6,46 @@ open SqlTokenReplacer.Types
 
 [<Test>]
 let CheckCommandSyntax_WillAcceptASingleVariable () =
-    let input = "#var[command()]#"
-    let result = checkCommandSyntax input
+    let input = "#var[command()]<int>#"
+    let result = commandSyntaxIsOk input
     Assert.That(result, Is.True)
 
 [<Test>]
 let CheckCommandSyntax_WillAcceptMultiVariables () =
-    let input = "#var1,var2[command()]#"
-    let result = checkCommandSyntax input
+    let input = "#var1,var2[command()]<string>#"
+    let result = commandSyntaxIsOk input
     Assert.That(result, Is.True)
 
 
 [<Test>]
 let CheckCommandSyntax_WillRejectNoVariables () =
-    let input = "#[command()]#"
-    let result = checkCommandSyntax input
+    let input = "#[command()]<string>#"
+    let result = commandSyntaxIsOk input
     Assert.That(result, Is.False)
 
 [<Test>]
 let CheckCommandSyntax_WillAcceptSingleArg () =
-    let input = "#var1[command(arg1)]#"
-    let result = checkCommandSyntax input
+    let input = "#var1[command(arg1)]<int>#"
+    let result = commandSyntaxIsOk input
     Assert.That(result, Is.True)
 
 [<Test>]
 let CheckCommandSyntax_WillAcceptMultpleArgs () =
-    let input = "#var1[command(arg1,arg2)]#"
-    let result = checkCommandSyntax input
+    let input = "#var1[command(arg1,arg2)]<string>#"
+    let result = commandSyntaxIsOk input
     Assert.That(result, Is.True)
 
 [<Test>]
 let CheckCommandSyntax_WillRejectMultipleCommands () =
-    let input = "#var1[command,command2(arg1,arg2)]#"
-    let result = checkCommandSyntax input
+    let input =
+        "#var1[command,command2(arg1,arg2)]<int>#"
+
+    let result = commandSyntaxIsOk input
     Assert.That(result, Is.False)
 
 [<Test>]
 let GetVariableArgs_WillGetMatchingVariables () =
-    let cmdStr = "#var[command()]#"
+    let cmdStr = "#var[command()]<string>#"
 
     let variables =
         [ { Content = [| "some content" |]
@@ -61,7 +63,7 @@ let GetVariableArgs_WillGetMatchingVariables () =
 
 [<Test>]
 let GetVariableArgs_WillReturnEmptyListIfNoMatches () =
-    let cmdStr = "#var3[command()]#"
+    let cmdStr = "#var3[command()]<string>#"
 
     let variables =
         [ { Content = [| "some content" |]
@@ -77,7 +79,7 @@ let GetVariableArgs_WillReturnEmptyListIfNoMatches () =
 
 [<Test>]
 let GetVariableArgs_WillReturnEmptyArray_WithEmptyVariablesInput () =
-    let cmdStr = "#var3[command()]#"
+    let cmdStr = "#var3[command()]<string>#"
     let variables = []
     let result = getVariableArgs cmdStr variables
     let expected = []
@@ -86,7 +88,7 @@ let GetVariableArgs_WillReturnEmptyArray_WithEmptyVariablesInput () =
 
 [<Test>]
 let GetCmdArgs_WillReturnArgList () =
-    let cmdStr = "#var3[command(a,b,c,d)]#"
+    let cmdStr = "#var3[command(a,b,c,d)]<string>#"
     let result = getCmdArgs cmdStr
     let expected = [ "a"; "b"; "c"; "d" ]
 
@@ -94,44 +96,49 @@ let GetCmdArgs_WillReturnArgList () =
 
 [<Test>]
 let GetCmdArgs_WillReturnEmptyListNoArgs () =
-    let cmdStr = "#var3[command()]#"
+    let cmdStr = "#var3[command()]<int>#"
     let result = getCmdArgs cmdStr
     Assert.That(result.Length, Is.EqualTo(0))
 
 [<Test>]
 let GetCmdType_WillGetAll () =
-    let cmdStr = "#var[All()]#"
+    let cmdStr = "#var[All()]<string>#"
     let result = getCmdType cmdStr
     Assert.That(result, Is.EqualTo(Some All))
 
 [<Test>]
 let GetCmdType_WillGetList () =
-    let cmdStr = "#var[List()]#"
+    let cmdStr = "#var[List()]<string>#"
     let result = getCmdType cmdStr
     Assert.That(result, Is.EqualTo(Some List))
 
 [<Test>]
 let GetCmdType_WillGetSingle () =
-    let cmdStr = "#var[Single()]#"
+    let cmdStr = "#var[Single()]<string>#"
     let result = getCmdType cmdStr
     Assert.That(result, Is.EqualTo(Some Single))
 
 [<Test>]
 let GetCmdType_WillGetWhereList () =
-    let cmdStr = "#var[WhereList()]#"
+    let cmdStr = "#var[WhereList()]<string>#"
     let result = getCmdType cmdStr
     Assert.That(result, Is.EqualTo(Some WhereList))
 
 [<Test>]
 let GetCmdType_WillGetWhereZip () =
-    let cmdStr = "#var[WhereZip()]#"
+    let cmdStr = "#var[WhereZip()]<string>#"
     let result = getCmdType cmdStr
-    Assert.That(result, Is.EqualTo(Some WhereZip))
+    Assert.That(result, Is.EqualTo(Some FlexZip))
 
 [<Test>]
 let GetCmdType_WillReturnNone () =
-    let cmdStr = "#var[SDASfasdas()]#"
+    let cmdStr = "#var[SDASfasdas()]<string>#"
     let result = getCmdType cmdStr
     Assert.That(result, Is.EqualTo(None))
 
-// todo check interpetTokens 
+
+[<Test>]
+let GetType_WillGetTypeInBrackets () =
+    let cmdStr = "#var[command()]<string>#"
+    let result = getType cmdStr
+    Assert.That(result, Is.EqualTo("string"))
